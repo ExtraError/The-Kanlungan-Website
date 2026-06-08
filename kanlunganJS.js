d884d937ad4b970547ceb935ba0eca1c6052ac3d
Jogie <jbahonsua11@gmail.com>
2026-06-09 02:45:07 +0300

    hamburger works in main page and other page and fixing about us page css

--- kanlunganJS.js
+++ kanlunganJS.js
@@ -1,26 +1,17 @@
-const hamburger = document.getElementById("hamburgerBtn");
-const navMenu = document.getElementById("navMenu");
-
-hamburger.addEventListener("click", (e) => {
-  e.stopPropagation();
-  navMenu.classList.toggle("active");
-});
-
-document.addEventListener("click", (e) => {
-  if (!navMenu.contains(e.target) && !hamburger.contains(e.target)) {
-    navMenu.classList.remove("active");
-  }
-});
-
-
-
 function initHamburger() {
-  const btn = document.getElementById("hamburgerBtn");
-  const nav = document.getElementById("navMenu");
+  const hamburger = document.getElementById("hamburgerBtn");
+  const navMenu = document.getElementById("navMenu");
 
-  if (!btn || !nav) return;
+  if (!hamburger || !navMenu) return;
 
-  btn.addEventListener("click", () => {
-    nav.classList.toggle("active");
+  hamburger.addEventListener("click", (e) => {
+    e.stopPropagation();
+    navMenu.classList.toggle("active");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!navMenu.contains(e.target) && !hamburger.contains(e.target)) {
+      navMenu.classList.remove("active");
+    }
   });
 }
