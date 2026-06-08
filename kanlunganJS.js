c0b73096413d0efc639a4eb5c60ade7d39a36a57
Jogie <jbahonsua11@gmail.com>
2026-06-08 22:24:49 +0300

    updating menu and sub menu in mobile view

--- kanlunganJS.js
+++ kanlunganJS.js
@@ -1,6 +1,13 @@
-const btn = document.getElementById("hamburgerBtn");
-const nav = document.getElementById("navMenu");
+const hamburger = document.getElementById("hamburgerBtn");
+const navMenu = document.getElementById("navMenu");
 
-btn.addEventListener("click", () => {
-  nav.classList.toggle("active");
+hamburger.addEventListener("click", (e) => {
+  e.stopPropagation();
+  navMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+  if (!navMenu.contains(e.target) && !hamburger.contains(e.target)) {
+    navMenu.classList.remove("active");
+  }
 });
