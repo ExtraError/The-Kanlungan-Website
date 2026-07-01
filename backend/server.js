8e1c7ccb7716bf87d259fd47543a4c84f36f51b8
ExtraError <jbahonsua11@gmail.com>
2026-07-01 10:00:23 +0800

    mail fails, mail test 1

--- backend/server.js
+++ backend/server.js
@@ -104,7 +104,9 @@ ${message}
 // google mail transporter
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
