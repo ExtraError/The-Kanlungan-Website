e564f1a9dcb6a0e2379a1c7867635416ed7675de
ExtraError <jbahonsua11@gmail.com>
2026-07-02 08:17:00 +0800

    adding url to backend CORS

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // IMPORTANT: lets backend read form data (JSON)
 app.use(cors({
-    origin: "*"
+    origin: "https://extraerror.github.io/The-Kanlungan-Website/"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
