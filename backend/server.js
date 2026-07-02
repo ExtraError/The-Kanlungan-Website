d822be001f1967212a9b9589366d7168b3a89298
ExtraError <jbahonsua11@gmail.com>
2026-07-02 11:15:00 +0800

    test6

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,12 @@ const PORT = process.env.PORT || 3000;
 
 // IMPORTANT: lets backend read form data (JSON)
 app.use(cors({
-    origin: "https://extraerror.github.io",
-            "https://extraerror.github.io/The-Kanlungan-Website"
+    origin: [
+        "https://extraerror.github.io",
+        "https://extraerror.github.io/The-Kanlungan-Website"
+    ]
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
