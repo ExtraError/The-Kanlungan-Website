d79d78aae9deebf31ad47935ad5e8b5dc13a1419
ExtraError <jbahonsua11@gmail.com>
2026-07-02 08:48:43 +0800

    checkin test2

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-
 const nodemailer = require("nodemailer");
 
 const cors = require("cors");
